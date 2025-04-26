6c305d172817feaa29bfd8d432c029b962d14746
0xKash <wwwkash2@gmail.com>
2025-04-26 15:40:14 +0200

    fix: fix postlayout component

--- src/components/postpreview-card.jsx
+++ src/components/postpreview-card.jsx
@@ -1,9 +1,9 @@
 const PostPreview = ({ title }) => {
   return (
-    <div className="card card-dash bg-base-100 w-200 shadow-md transition hover:scale-101">
+    <div className="card card-dash bg-base-100  shadow-md transition hover:scale-101">
       <div className="card-body">
         <h2 className="card-title text-2xl">{title}</h2>
-        <div className="flex w-full ">
+        <div className="flex">
           <p className="font-bold">@Kash</p>
           <p className="font-bold flex justify-end">26/04/2025</p>
         </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,14 +3,17 @@ import PostPreview from "../components/postpreview-card";
 
 const Home = () => {
   return (
-    <div className="flex items-center flex-col gap-10 p-10">
-      <PostPreview title={"My first post!"} />
-      <PostPreview title={"My second post!"} />
-      <PostPreview title={"My third post!"} />
-      <PostPreview title={"My first post!"} />
-      <PostPreview title={"My second post!"} />
-      <PostPreview title={"My third post!"} />
-    </div>
+    <>
+      <Navbar />
+      <div className="flex items-center flex-col gap-10 p-10">
+        <PostPreview title={"My first post!"} />
+        <PostPreview title={"My second post!"} />
+        <PostPreview title={"My third post!"} />
+        <PostPreview title={"My first post!"} />
+        <PostPreview title={"My second post!"} />
+        <PostPreview title={"My third post!"} />
+      </div>
+    </>
   );
 };
 
